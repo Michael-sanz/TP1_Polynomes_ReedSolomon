cb9f8ba1a3b91d803dd8e64bb2da58787aad8c32
Buscaape <dylan.soares.24@gmail.com>
2021-12-02 18:41:25 +0100

    Ajout de la division entière dans la fonction de Kevin. Ajout de la variable de liste

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+liste=[67, 104, 38, 124, 113, 114, 132, 101, 116, 59, 101, 115, 152, 32, 151, 105, 110, 148, 98, 108, 101, 54, 97, 74, 85, 250, 141, 198, 244, 211, 18, 113, 172, 236, 191, 138, 68, 79, 198, 132, 167, 17, 86]
+
 def euclide_etendue(a, b):
     if a==0:
         return b,0,1
@@ -10,7 +12,7 @@ def euclide_etend_Kex(a,b):
         return a,1,0
     else:
         (pgcd,u,v)= euclide_etend_Kex(b, a%b)
-        return (pgcd,v,u-(a/b)*v)
+        return (pgcd,v,u-(a//b)*v)
 
 # fonction euclide-étendu(a, b)
 #     si b = 0 alors
