aca7e9058ff5b3ac7e5b142d6959ad2034a52aac
Buscaape <dylan.soares.24@gmail.com>
2021-12-02 19:38:59 +0100

    ajout code manquant pour addition.

--- main.py
+++ main.py
@@ -14,8 +14,21 @@ def euclide_etend_Kex(a,b):
         (pgcd,u,v)= euclide_etend_Kex(b, a%b)
         return (pgcd,v,u-(a//b)*v)
 
+def voirDiff(polynome1, polynome2):
+    if len(polynome1) - len(polynome2) < 0:
+        diff = len(polynome2) - len(polynome1)
+        ajouterManquant(polynome1, diff)
+    else:
+        diff = len(polynome1) - len(polynome2)
+        ajouterManquant(polynome2, diff)
+
+def ajouterManquant(poly,diff):
+    for i in range(diff):
+        poly.append(0)
+
 def ajouterPolynome(polynome1, polynome2):
     newPoly=[]
+    voirDiff(polynome1, polynome2)
     for x in range(len(polynome1)):
         newPoly.append(polynome1[x]+polynome2[x])
     return newPoly
@@ -49,5 +62,7 @@ def grange(points):
         L = ajouterPolynome(l_j,L)
     return L
 
-print(euclide_etend_Kex(235,7))
-print(modinv(873,602))
+
+valeur =[[0,1],[1,3],[2,7]]
+
+print(grange(valeur))
