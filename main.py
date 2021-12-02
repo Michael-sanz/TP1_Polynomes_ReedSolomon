21c1743ce04c288d012031412371e2023b6ac22d
kexbng6 <kevin.bonga@gmail.com>
2021-12-02 16:43:36 +0100

    nouvel algo d'euclide etendue

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ def euclide_etendue(a, b):
     else:
         gcd,x,y=euclide_etendue(b % a, a)
         return gcd, y-(b//a)*x,x
+
+def euclide_etend_Kex(a,b):
+    if (b==0):
+        return a,1,0
+    else:
+        (pgcd,u,v)= euclide_etend_Kex(b, a%b)
+        return (pgcd,v,u-(a/b)*v)
+
+# fonction euclide-étendu(a, b)
+#     si b = 0 alors
+#           retourner (a, 1, 0)
+#     sinon
+#           (d', u', v') := euclide-étendu(b, a mod b)
+#           retourner (d', v', u' - (a÷b)v')
