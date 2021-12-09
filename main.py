b8f7ab4339f2d24413f59eb4cc876ece4921b25c
michael <elv-michael.snz@eduge.ch>
2021-12-09 19:29:26 +0100

    returnGrange + ADD CONST GLOBAL

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-liste=[67, 104, 38, 124, 113, 114, 132, 101, 116, 59, 101, 115, 152, 32, 151, 105, 110, 148, 98, 108, 101, 54, 97, 74, 85, 250, 141, 198, 244, 211, 18, 113, 172, 236, 191, 138, 68, 79, 198, 132, 167, 17, 86]
+from itertools import combinations
+
+liste_initial=[67, 104, 38, 124, 113, 114, 132, 101, 116, 59, 101, 115, 152, 32, 151, 105, 110, 148, 98, 108, 101, 54, 97, 74, 85, 250, 141, 198, 244, 211, 18, 113, 172, 236, 191, 138, 68, 79, 198, 132, 167, 17, 86]
+
+# GLOBAL CONST
 
 NB_PREMIER = 257
+NB_POINTS_JUSTE = -19
+NB_POINTS_CONTENANT_ERREURS = 24
 
 def euclide_etendu(a,b):
     if (b==0):
@@ -63,8 +69,20 @@ def convert(lst):
         dictionnary += [[i,j]]
     return dictionnary
 
+def returnGrange(list):
+    listeJuste = convert(list)[NB_POINTS_JUSTE:]
+    randomList = combinations(list[:NB_POINTS_CONTENANT_ERREURS], 2)
+    for i in randomList:
+        # print(convert(i))
+        new_list = listeJuste + convert(i)
+        print(lagrange(new_list))
+        # print(new_list)
+
+
+
+
 valeur =[[0,1],[1,3],[2,7]]
 
 if __name__ == '__main__':
-    print(lagrange(valeur))
+    returnGrange(liste_initial)
     #print("Bonsoir Paris")
