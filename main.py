a6cfa78c5c6bb635dd70fcde5aed474bb4bbdbd7
dylanmntrs <elv-dylan.mntrs@eduge.ch>
2021-12-19 15:05:41 +0100

    Ajout de commentaires ainsi que de la fonction pour convertir les chiffres en caractères

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from itertools import combinations
 
-liste_initial=[67, 104, 38, 124, 113, 114, 132, 101, 116, 59, 101, 115, 152, 32, 151, 105, 110, 148, 98, 108, 101, 54, 97, 74, 85, 250, 141, 198, 244, 211, 18, 113, 172, 236, 191, 138, 68, 79, 198, 132, 167, 17, 86]
 
-# GLOBAL CONST
-
+"""
+GLOBAL CONST
+"""
+LISTE_INITIAL=[67, 104, 38, 124, 113, 114, 132, 101, 116, 59, 101, 115, 152, 32, 151, 105, 110, 148, 98, 108, 101, 54, 97, 74, 85, 250, 141, 198, 244, 211, 18, 113, 172, 236, 191, 138, 68, 79, 198, 132, 167, 17, 86]
 NB_PREMIER = 257
 NB_POINTS_JUSTE = -19
 NB_POINTS_CONTENANT_ERREURS = 24
 
 
 def euclide_etendu(a,b):
+    """
+    Fonction d'euclide étendu qui retourne le pgcd, le v et le u de 2 nombre
+    :param a: premier nombre afin de trouver le pgcd
+    :param b: deuxième nombre afin de trouver le pgcd
+    :return: le pgcd, le v de a et le u de b
+    """
     if (b==0):
         return a,1,0
     else:
@@ -17,6 +24,11 @@ def euclide_etendu(a,b):
         return (pgcd,v,u-(a//b)*v)
 
 def voirDiff(polynome1, polynome2):
+    """
+    Fonction qui regarde la différence de taille d'un polynome et qui complète par des 0 afin que les 2 polynomes soient de même taille
+    :param polynome1: un polynome
+    :param polynome2: un polynome
+    """
     if len(polynome1) - len(polynome2) < 0:
         diff = len(polynome2) - len(polynome1)
         ajouterManquant(polynome1, diff)
@@ -25,10 +37,21 @@ def voirDiff(polynome1, polynome2):
         ajouterManquant(polynome2, diff)
 
 def ajouterManquant(poly,diff):
+    """
+    Ajoute un 0 à un polynome
+    :param poly: un polynome
+    :param diff: le nombre de 0 à ajouter
+    """
     for i in range(diff):
         poly.append(0)
 
-def ajouterPolynome(polynome1, polynome2):
+def additionPolynome(polynome1, polynome2):
+    """
+    Fonction d'addition de polynome
+    :param polynome1: un polynome
+    :param polynome2: un polynome
+    :return: l'addition des 2 polynomes en paramètre
+    """
     newPoly=[]
     voirDiff(polynome1, polynome2)
     for x in range(len(polynome1)):
@@ -43,6 +66,12 @@ def modinv(a, m):
         return x % m
 
 def multiplierPolynome(polynome1, polynome2):
+    """
+    Fonction de multiplication de polynome
+    :param polynome1: un polynome
+    :param polynome2: un polynome
+    :return: la multiplication des 2 polynomes en paramètre
+    """
     newPoly=[]
     for i in range((len(polynome1)+len(polynome2)-1)):
         newPoly.append(0)
@@ -52,6 +81,11 @@ def multiplierPolynome(polynome1, polynome2):
     return newPoly
 
 def lagrange(points):
+    """
+    Fonction qui calcule la grande pour une liste de points
+    :param points: la liste de points à calculer
+    :return: la liste de points en paramètre après été calculer par la grange
+    """
     L =[0]
     for i in points:
         l_j = [1]
@@ -61,16 +95,26 @@ def lagrange(points):
                 polyDivision = [(-1)*j[0]*modinv(reste, NB_PREMIER), 1*modinv(reste, NB_PREMIER)]
                 l_j=multiplierPolynome(l_j, polyDivision)
         l_j =multiplierPolynome(l_j,[i[1]])
-        L = ajouterPolynome(l_j,L)
+        L = additionPolynome(l_j, L)
     return L
 
 def convert(lst):
+    """
+    Converti une liste en plusieurs listes de points
+    :param lst: une liste
+    :return: un dictionnaire de liste avec des indices
+    """
     dictionnary = []
     for i,j in enumerate(lst):
         dictionnary += [[i,j]]
     return dictionnary
 
-def returnGrange(list):
+def returnBonneListe(list):
+    """
+    Retourne la bonne liste de points passant par les 32 points afin de pouvoir décoder le message
+    :param list: la liste initiale
+    :return: la liste de points correcte
+    """
     listeJuste = convert(list)[24:]
     randomList = combinations(list[:NB_POINTS_CONTENANT_ERREURS], 2)
     grange_list = lagrange(convert(list))
@@ -81,12 +125,20 @@ def returnGrange(list):
         for i in range(len(grange_list)):
             if(evalPoly(L,i)==list[i]):
                 cpt+=1
+
         if(cpt==32):
-            print(new_list)
+            return new_list
+            break
 
         # evalPoly(lagrange(new_list),10)
 
 def evalPoly(pol,x):
+    """
+    Fonction d'évaluation de polynome
+    :param pol: liste à évaluer
+    :param x: chiffre pour le calcul de l'évaluation
+    :return: le chiffre de cette évaluation
+    """
     somme=0
     for degre, coef in enumerate(pol[::-1]):
         if degre==0:
@@ -94,11 +146,17 @@ def evalPoly(pol,x):
         else:
             somme=(somme*x+coef)%NB_PREMIER
     return somme
-if __name__ == '__main__':
-    returnGrange(liste_initial)
 
-    # Return grange retourne une liste de polynome ex: [[0,86]...]->[[n,m]]
-    # passer dans eval poly 'm' (evalpoly([returnGrange(liste_initial)->new_list],n))
-    # comparer si le m en indice n correspond à celui de la new list et si oui alors:
-    # cpt+=(p(i)==L[i])
-    # si cpt == 32 alors bon polynome et le retourner
+def toPhrase(List):
+    """
+    Converti une liste de points en caractère
+    :param List: une liste de points
+    """
+    phrase = ""
+    for i in List:
+        phrase += chr(i[1])
+        print(chr(i[1]))
+
+
+if __name__ == '__main__':
+    print(toPhrase(returnBonneListe(LISTE_INITIAL)))
